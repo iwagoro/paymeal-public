e77739e61b08a6aa9e14e8f74c0bb6245e3b2ee1
ROCKWELL <rockwell@ROCKWELLnoMacBook-Air.local>
2024-04-06 21:16:06 -0700

    fixed layout

--- components/util/baseLayout/bottom-bar.tsx
+++ components/util/baseLayout/bottom-bar.tsx
@@ -2,14 +2,23 @@
 
 import Link from "next/link";
 import { useTheme } from "next-themes";
-import { GoHome } from "react-icons/go";
+import { GoHome, GoSearch, GoBell, GoHistory } from "react-icons/go";
 
 export const BottomBar = () => {
     const { theme, setTheme } = useTheme();
     return (
         <div className="absolute bottom-0 z-50 max-w-2xl w-full h-[50px] flex justify-between items-center px-10  gap-10 bg-transparent">
             <Link href="/home">
-                <GoHome size={24} />
+                <GoHome size={18} />
+            </Link>
+            <Link href="/menu">
+                <GoSearch size={18} />
+            </Link>
+            <Link href="/notification">
+                <GoBell size={18} />
+            </Link>
+            <Link href="/history">
+                <GoHistory size={18} />
             </Link>
         </div>
     );
