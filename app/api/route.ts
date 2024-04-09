2cbd03084629b0c545326000268bce3abd1b0d8c
ROCKWELL <rockwell@ROCKWELLnoMacBook-Air.local>
2024-04-08 17:50:34 -0700

    deleted api

--- app/api/route.ts
+++
@@ -1,8 +0,0 @@
-export async function GET(req: Request) {
-    const url = "http://127.0.0.1:8081/create_payment";
-
-    // const url = `https://api.openweathermap.org/data/2.5/weather?q=Tokyo&appid`;
-    const response = await fetch(url);
-    const paymentUrl = await response.json();
-    return Response.json({ paymentUrl });
-}
